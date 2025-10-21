5dc834a5f5d7f2d841acca1075883e9091b195aa
Hope Nzete <megamindsetsolutions@gmail.com>
2025-10-22 00:16:25 +0100

    Used satalited view

--- backend/map.php
+++ backend/map.php
@@ -91,21 +91,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                         // }).addTo(map);
 
-                        // L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-                        //     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-                        // }).addTo(map);
+                        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+                            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                        }).addTo(map);
 
 
-                        // L.tileLayer.wms("http://ows.mundialis.de/services/service?", {
-                        //     layers: "TOPO-OSM-WMS",
-                        //     format: "image/png",
-                        //     transparent: true
-                        // }).addTo(map);
 
-                        L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
-                            maxZoom: 20,
-                            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-                        });
+
+                        // L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
+                        //     maxZoom: 20,
+                        //     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+                        // });
                     </script>
                 <?php else : ?>
 
